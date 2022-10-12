5e7d0b0bd7910312020c867944591ff8121e1ef7
julian45 <julian45@users.noreply.github.com>
2022-10-12 17:55:13 +0000

    Prettified Code!

--- terribleformatting.js
+++ terribleformatting.js
@@ -1,20 +1,24 @@
-      $("#speedPercent").on("input", event => {
-  $(".output").value =      event.target.value + "%"    ;
-})
+$("#speedPercent").on("input", (event) => {
+  $(".output").value = event.target.value + "%";
+});
 
-$("#grid").on("click", event => {
-if (event.target && event.target.matches("button.banana")) {
-
-    const points =     parseInt(   event.target.dataset.points, 10     )  ;
-state = {        ...state, score: state.score + points };
+$("#grid").on("click", (event) => {
+  if (event.target && event.target.matches("button.banana")) {
+    const points = parseInt(event.target.dataset.points, 10);
+    state = { ...state, score: state.score + points };
     setScoreInnerHTML(state);
 
-const span    =       event.target.querySelector("span");
+    const span = event.target.querySelector("span");
     span.classList.add("exit-animation");
-span.on('animationend', ()   =>    {
-event.target.parentNode.removeChild(event.target)})
-    }
-  })
+    span.on("animationend", () => {
+      event.target.parentNode.removeChild(event.target);
+    });
+  }
+});
 
-        const mutationObserver    = observer(state)
-mutationObserver.observe($("#grid"), {attributes: false,childList: true,subtree: true});
+const mutationObserver = observer(state);
+mutationObserver.observe($("#grid"), {
+  attributes: false,
+  childList: true,
+  subtree: true,
+});
